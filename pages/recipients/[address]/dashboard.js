2ce6c27c3681c0af17ae68da754069a1091b4895
Brendan Asselstine <mail.asselstine@gmail.com>
2019-07-05 11:23:24 -0700

    Tweaked _redirects and added RelayRecipient queries

--- pages/recipients/[address]/dashboard.js
+++ pages/recipients/[address]/dashboard.js
@@ -1,20 +1,37 @@
+import { DynamicApolloWrapper } from 'lib/components/DynamicApolloWrapper'
 import { Query } from 'react-apollo'
-import { queries } from 'dapp-core'
+import { recipientQuery } from 'lib/queries/recipientQuery'
 import { useRouter } from 'next/router'
 
-import { networkIdToName } from 'lib/utils/networkIdToName'
-
-function Home() {
+function Dashboard() {
   const router = useRouter()
   const { address } = router.query
 
   return (
     <div>
-      <p>
-        Recipient {address}
-      </p>
+        <p>
+          Recipient {address}
+          <DynamicApolloWrapper>
+            <Query query={recipientQuery} variables={{ recipientAddress: address }}>
+              {({ data, loading, error }) => {
+                if (error) {
+                  console.error(error)
+                  return error.message
+                } else if (loading) {
+                  console.log('loading...')
+                  return '...'
+                } else {
+                  console.log(data)
+                  return <>
+                    Recipient balance is {data.balance.toString()}
+                  </>
+                }
+              }}
+            </Query>
+          </DynamicApolloWrapper>
+        </p>
     </div>
   )
 }
 
-export default Home;
+export default Dashboard;
