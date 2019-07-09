2e2108ef01942dd1f215029dbff0f74e37d37ced
Brendan Asselstine <mail.asselstine@gmail.com>
2019-07-09 15:21:49 -0700

    Added Recipient and RelayHub index pages

---
+++ pages/relay-hubs/index.jsx
@@ -0,0 +1,37 @@
+import React from 'react'
+
+import { DynamicApolloWrapper } from 'lib/components/DynamicApolloWrapper';
+import { Query } from 'react-apollo'
+import { queries } from 'dapp-core'
+
+import { MainLayout } from 'lib/components/layout/MainLayout'
+import { LandingPageForm } from 'lib/components/LandingPageForm'
+import { networkIdToName } from 'lib/utils/networkIdToName'
+
+const RelayHubsIndex = function() {
+  return (
+    <MainLayout>
+      <DynamicApolloWrapper>
+        <p>
+          <Query query={queries.networkAccountQuery}>
+            {({ data, loading }) => {
+              console.log(data)
+              var network = 'loading...'
+              if (!loading) {
+                network =  networkIdToName(data.networkId)
+              }
+
+              return <>
+                This content is dynamically loaded from Ethereum network {network}
+              </>
+            }}
+          </Query>
+        </p>
+
+        <LandingPageForm />
+      </DynamicApolloWrapper>
+    </MainLayout>
+  )
+}
+
+export default RelayHubsIndex;
