c4f106471775d60a263cbc170c79a2279997a513
Brendan Asselstine <mail.asselstine@gmail.com>
2019-07-09 16:05:27 -0700

    Removed dynamic apollo wrapper from recipient and relay hub indexes

--- pages/recipients/index.jsx
+++ pages/recipients/index.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 
-import { DynamicApolloWrapper } from 'lib/components/DynamicApolloWrapper';
 import { AddressLinkForm } from 'lib/components/AddressLinkForm'
 import { MainLayout } from 'lib/components/layout/MainLayout'
 import { Section } from 'lib/components/layout/Section'
@@ -8,14 +7,12 @@ import { Section } from 'lib/components/layout/Section'
 const RecipientsIndex = function() {
   return (
     <MainLayout>
-      <DynamicApolloWrapper>
-        <Section>
-          <AddressLinkForm
-            title='Access Recipient'
-            formatUrl={(address) => `/recipients/${address}`}
-          />
-        </Section>
-      </DynamicApolloWrapper>
+      <Section>
+        <AddressLinkForm
+          title='Access Recipient'
+          formatUrl={(address) => `/recipients/${address}`}
+        />
+      </Section>
     </MainLayout>
   )
 }
--- pages/relay-hubs/index.jsx
+++ pages/relay-hubs/index.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 
-import { DynamicApolloWrapper } from 'lib/components/DynamicApolloWrapper';
 import { MainLayout } from 'lib/components/layout/MainLayout'
 import { AddressLinkForm } from 'lib/components/AddressLinkForm'
 import { Section } from 'lib/components/layout/Section'
@@ -8,14 +7,12 @@ import { Section } from 'lib/components/layout/Section'
 const RelayHubsIndex = function() {
   return (
     <MainLayout>
-      <DynamicApolloWrapper>
-        <Section>
-          <AddressLinkForm
-            title='Access Relay Hub'
-            formatUrl={(address) => `/relay-hubs/${address}`}
-          />
-        </Section>
-      </DynamicApolloWrapper>
+      <Section>
+        <AddressLinkForm
+          title='Access Relay Hub'
+          formatUrl={(address) => `/relay-hubs/${address}`}
+        />
+      </Section>
     </MainLayout>
   )
 }
