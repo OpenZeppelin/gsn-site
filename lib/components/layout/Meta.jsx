c51326495917830d560182d2d9e6a3730f9d2d6c
Charles Bergeron <chuck.bergeron@gmail.com>
2019-07-11 17:01:36 -0700

    * first pass favicon and meta / head tags

--- lib/components/layout/Meta.jsx
+++ lib/components/layout/Meta.jsx
@@ -3,8 +3,33 @@ import Head from 'next/head'
 export default () => (
   <div>
     <Head>
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <title>GSN - Gas Station Network</title>
+
+      <meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no" />
       <meta charSet="utf-8" />
+
+      <link rel="icon" type="image/png" href="/favicon.png" />
+
+      <meta name="theme-color" content="#282844" />
+      <meta name="description" content="GSN enables you to listen in on Ethereum and receive notifications in response. Whether it's token transfers or contract activity, GSN has your back." />
+      <meta name="keywords" content="deltacamp delta camp GSN ethereum smart contract notifications" />
+      <meta name="author" content="GSN" />
+      <meta name="copyright" content="&copy; 2019 GSN" />
+
+      <meta property="og:title" content="GSN - Gas Station Network" />
+      <meta property="og:description" content="GSN enables you to listen in on Ethereum and receive notifications in response. Whether it's token transfers or contract activity, GSN has your back." />
+      <meta property="og:site_name" content="GSN - Gas Station Network" />
+      <meta property="og:url" content="https://www.GSN.events/" />
+      <meta property="og:type" content="website" />
+      <meta property="og:image" content="https://www.GSN.events/facebook_share_image_1200x630.png" />
+      <meta property="og:rich_attachment" content="true" />
+      
+      <meta property="twitter:title" content="GSN - Gas Station Network" />
+      <meta property="twitter:card" content="summary_large_image" />
+      <meta property="twitter:site" content="@GSNEvents" />
+      <meta property="twitter:image:src" content="https://www.GSN.events/twitter_share_image_800_418.png" />
+      <meta property="twitter:url" content="https://twitter.com/GSNEvents" />
+      <meta property="twitter:creator" content="@GSNEvents" />
     </Head>
     <style jsx global>{`
       body { 
