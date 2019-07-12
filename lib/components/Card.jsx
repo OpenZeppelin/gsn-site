7358f51156299cd00f2e7b557f64ec0d8c2345ab
Brendan Asselstine <mail.asselstine@gmail.com>
2019-07-12 15:57:12 -0700

    Made whole card clickable

--- lib/components/Card.jsx
+++ lib/components/Card.jsx
@@ -6,7 +6,7 @@ export const Card = ({ img, text, url, large }) => {
   const verticalPadding = large ? 'py-6 sm:py-8 lg:pt-10 lg:pb-24' : 'py-6 sm:py-8'
 
   return (
-    <div className='relative flex-grow bg-white overflow-hidden shadow-md hover:shadow-lg trans trans-fast pb-20'>
+    <a href={url} target='_blank' rel='noreferrer' className='relative flex-grow bg-white overflow-hidden shadow-md hover:shadow-lg trans trans-fast lg:pb-10'>
       <div className='text-center flex justify-center p-6 lg:p-12 h-30 border-solid border-b border-gray-400'>
         {img}
       </div>
@@ -16,9 +16,6 @@ export const Card = ({ img, text, url, large }) => {
           {text}
         </p>
       </div>
-      <div className={`${horizontalPadding} pt-6 mb-12 absolute bottom-0`}>
-        <a className='font-silkaMedium uppercase text-sm' href={url}>Go To Link &gt;</a>
-      </div>
-    </div>
+    </a>
   )
 }
