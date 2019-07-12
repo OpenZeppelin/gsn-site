8c24a4030e03575dbbcc5aa04bdb720481d5a809
Charles Bergeron <chuck.bergeron@gmail.com>
2019-07-12 10:18:57 -0700

    * 2nd pass metatags

--- lib/components/layout/Meta.jsx
+++ lib/components/layout/Meta.jsx
@@ -1,40 +1,48 @@
 import Head from 'next/head'
 
-export default () => (
-  <div>
-    <Head>
-      <title>GSN - Gas Station Network</title>
+export default () => {
+  const url = `https://gsn-staging.netlify.com`
+  const description = 'The Gas Station Network is the final onboarding solution for Ethereum applications enabling you to pay for transaction gas fees on behalf of your users.'
+  const keywords = 'zeppelin openzeppelin tabookey gsn gas station network deltacamp delta camp ethereum smart contract'
+  const title = 'Gas Station Network'
+  const twitterHandle = '@GSN'
 
-      <meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no" />
-      <meta charSet="utf-8" />
+  return (
+    <div>
+      <Head>
+        <title>GSN - Gas Station Network</title>
 
-      <link rel="icon" type="image/png" href="/favicon.png" />
+        <meta name='viewport' content='width=device-width,initial-scale=1,shrink-to-fit=no' />
+        <meta charSet='utf-8' />
 
-      <meta name="theme-color" content="#282844" />
-      <meta name="description" content="GSN enables you to listen in on Ethereum and receive notifications in response. Whether it's token transfers or contract activity, GSN has your back." />
-      <meta name="keywords" content="deltacamp delta camp GSN ethereum smart contract notifications" />
-      <meta name="author" content="GSN" />
-      <meta name="copyright" content="&copy; 2019 GSN" />
+        <link rel='icon' type='image/png' href='/favicon.png' />
 
-      <meta property="og:title" content="GSN - Gas Station Network" />
-      <meta property="og:description" content="GSN enables you to listen in on Ethereum and receive notifications in response. Whether it's token transfers or contract activity, GSN has your back." />
-      <meta property="og:site_name" content="GSN - Gas Station Network" />
-      <meta property="og:url" content="https://www.GSN.events/" />
-      <meta property="og:type" content="website" />
-      <meta property="og:image" content="https://www.GSN.events/facebook_share_image_1200x630.png" />
-      <meta property="og:rich_attachment" content="true" />
-      
-      <meta property="twitter:title" content="GSN - Gas Station Network" />
-      <meta property="twitter:card" content="summary_large_image" />
-      <meta property="twitter:site" content="@GSNEvents" />
-      <meta property="twitter:image:src" content="https://www.GSN.events/twitter_share_image_800_418.png" />
-      <meta property="twitter:url" content="https://twitter.com/GSNEvents" />
-      <meta property="twitter:creator" content="@GSNEvents" />
-    </Head>
-    <style jsx global>{`
-      body { 
+        <meta name='theme-color' content='#282844' />
+        <meta name='description' content={description} />
+        <meta name='keywords' content={keywords} />
+        <meta name='author' content='GSN' />
+        <meta name='copyright' content='&copy; 2019' />
 
-      }
-    `}</style>
-  </div>
-)
+        <meta property='og:title' content={title} />
+        <meta property='og:description' content={description} />
+        <meta property='og:site_name' content={title} />
+        <meta property='og:url' content={url} />
+        <meta property='og:type' content='website' />
+        <meta property='og:image' content={`${url}/facebook_share_image_1200x630.png`} />
+        <meta property='og:rich_attachment' content='true' />
+        
+        <meta property='twitter:title' content={title} />
+        <meta property='twitter:card' content='summary_large_image' />
+        <meta property='twitter:site' content={twitterHandle} />
+        <meta property='twitter:image:src' content={`${url}/twitter_share_image_800_418.png`} />
+        <meta property='twitter:url' content={url} />
+        <meta property='twitter:creator' content={twitterHandle} />
+      </Head>
+      {/* <style jsx global>{`
+        body { 
+
+        }
+      `}</style> */}
+    </div>
+  )
+}
