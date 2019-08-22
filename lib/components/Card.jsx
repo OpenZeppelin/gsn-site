8800a15014e3de399494d756c1a46443713db057
Ramon Recuero <rrmoreno1987@gmail.com>
2019-08-22 10:01:17 +0200

    Mobile fixes

--- lib/components/Card.jsx
+++ lib/components/Card.jsx
@@ -7,7 +7,7 @@ export const Card = ({ img, text, url, large }) => {
   const verticalPadding = large ? 'py-6 sm:py-8 lg:pt-10 lg:pb-24' : 'py-6 sm:py-8'
 
   return (
-    <a href={url} target='_blank' rel='noreferrer' className='relative flex-grow bg-white overflow-hidden shadow-md hover:shadow-lg trans trans-fast lg:pb-10'>
+    <a href={url} target='_blank' rel='noreferrer' className='relative flex-grow bg-white overflow-hidden shadow-md hover:shadow-lg trans trans-fast lg:pb-10 py-5'>
       <div className='text-center flex justify-center p-6 lg:p-12 h-30 border-solid border-b border-gray-400'>
         {img}
       </div>
